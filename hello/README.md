ff7eae2c884ad2d3dcabac0ff2e7252e6a40d9e0
Chenjiff <1004920224@qq.com>
2019-12-03 02:44:29 +0800

    init

---
+++ hello/README.md
@@ -0,0 +1,4 @@
+# HelloGo
+service calculator
+
+ Program for the week3 homework 
---
+++ hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Printf("Hello, world.\n")
+// }
+import (
+	"fmt"
+	"github.com/user/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+}
